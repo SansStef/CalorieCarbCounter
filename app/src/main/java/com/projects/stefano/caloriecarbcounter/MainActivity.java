71053d24ec89c3202123cfae158b9b4f2486186b
Stefano <ses8516@rit.edu>
2016-03-19 03:13:32 -0400

    Create custom dialog for entering food log Change launcher icons to CCC logo Add string resources

--- app/src/main/java/com/projects/stefano/caloriecarbcounter/MainActivity.java
+++ app/src/main/java/com/projects/stefano/caloriecarbcounter/MainActivity.java
@@ -1,15 +1,21 @@
 package com.projects.stefano.caloriecarbcounter;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,10 +31,34 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                showFoodDialog();
             }
         });
+    }
+
+    protected void showFoodDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        LayoutInflater inflater = MainActivity.this.getLayoutInflater();
+        View dialog = inflater.inflate(R.layout.add_food, null);
+
+        builder.setView(dialog)
+                .setTitle(R.string.add_food_title);
+
+        //Add food views
+        final EditText editText_Food = (EditText) dialog.findViewById(R.id.editText_food);
+        final TimePicker timePicker_Food = (TimePicker) dialog.findViewById(R.id.timePicker_food);
+
+        // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //Food dialog variables
+                String m_Text = editText_Food.getText().toString();
+            }
+        });
+        builder.setNegativeButton("Cancel", null);
+
+        builder.show();
     }
 
     @Override
